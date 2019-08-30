6e2cfb0156dac393c0b75947e72496907f6f9775
Pon Cheol Ku <bbon@bbon.kr>
2019-08-30 22:49:43 +0900

    ADDED: - express 준비 - sequelize 준비

---
+++ src/models/index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    sequelize = new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config,
+    );
+}
+
+
+db.User = require('./User')(sequelize, Sequelize);
+
+/** 
+ * passport store 
+ */
+db.Session = require('../passport/session')(sequelize, Sequelize);
+
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
